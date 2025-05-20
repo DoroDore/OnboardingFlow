90f739449f0950e86e018a642a0867d4a7da8946
DoroDore <tlyz82@student.cis.eduhk>
2025-05-20 10:03:26 +0800

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Theodore Zhu on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OnboardingFlowTests/OnboardingFlowTests.swift
@@ -0,0 +1,16 @@
+//
+//  OnboardingFlowTests.swift
+//  OnboardingFlowTests
+//
+//  Created by Theodore Zhu on 5/20/25.
+//
+
+import Testing
+
+struct OnboardingFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
